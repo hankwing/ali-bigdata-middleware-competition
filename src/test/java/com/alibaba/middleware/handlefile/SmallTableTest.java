963863ec85a80b9caadbfcfb8610446ad55c4abc
Jin Guodong <jelly.guodong.jin@gmail.com>
2016-07-17 16:36:25 +0800

    stash

---
+++ src/test/java/com/alibaba/middleware/handlefile/SmallTableTest.java
@@ -0,0 +1,13 @@
+package com.alibaba.middleware.handlefile;
+
+/**
+ * order-system-impl-master-cabe626d3eb46a36ae1f74a33ef3e8c7182536c7order-system-impl.git
+ *
+ * @author Jelly
+ */
+public class SmallTableTest {
+
+    public static void main(String[] args) {
+
+    }
+}
