4363f2a7572a416f6ff74a696049234461343815
USER <USER@USER-PC>
2016-07-18 08:56:42 +0800

    多个单线程读写

--- src/main/java/com/alibaba/middleware/handlefile/ConstructSystem.java
+++ src/main/java/com/alibaba/middleware/handlefile/ConstructSystem.java
@@ -10,16 +10,94 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class ConstructSystem {
 
-	CountDownLatch countDownLatch;
+	//代理映射表
 	AgentMapping agentBuyerMapping;
 	AgentMapping agentGoodMapping;
+
+	class BuyerRun implements Runnable{
+		CountDownLatch countDownLatch;
+		List<String> files;
+		public BuyerRun(CountDownLatch countDownLatch, List<String> files) {
+			this.countDownLatch = countDownLatch;
+			this.files = files;
+		}
+		public void run() {
+			// TODO Auto-generated method stub
+			
+			BuyerHandler buyerHandler = new BuyerHandler(agentBuyerMapping);
+			buyerHandler.handeBuyerFiles(files);
+			countDownLatch.countDown();
+		}
+	}
+
+	class GoodRun implements Runnable{
+		CountDownLatch countDownLatch;
+		List<String> files;
+		public GoodRun(CountDownLatch countDownLatch, List<String> files) {
+			this.countDownLatch = countDownLatch;
+			this.files = files;
+		}
+		public void run() {
+			// TODO Auto-generated method stub
+			GoodHandler goodHandler = new GoodHandler(agentGoodMapping);
+			goodHandler.HandleGoodFiles(files);
+			countDownLatch.countDown();
+		}
+	}
+
+	class OrderRun implements Runnable{
+		CountDownLatch countDownLatch;
+		List<String> files;
+		public OrderRun(CountDownLatch countDownLatch, List<String> files) {
+			this.countDownLatch = countDownLatch;
+			this.files = files;
+		}
+		public void run() {
+			// TODO Auto-generated method stub
+			OrderHandler orderHandler = new OrderHandler(agentGoodMapping, agentBuyerMapping);
+			orderHandler.HandleOrderFiles(files);
+			countDownLatch.countDown();
+		}
+	}
 
 	public ConstructSystem() {
 		agentBuyerMapping = new AgentMapping();
 		agentGoodMapping = new AgentMapping();
 	}
 
-	private List<String> getDealingFiles(List<String> files, int group ,int readers){
+	public void startHandling(List<String> buyerfiles,List<String> goodfiles,List<String> orderfiles,int threadNum){
+		CountDownLatch countDownLatch;
+		try {
+			countDownLatch = new CountDownLatch(threadNum);
+			for (int i = 0; i < threadNum; i++) {
+				final List<String> files = getGroupFiles(buyerfiles,i,threadNum);
+				new Thread(new BuyerRun(countDownLatch, files)).start();;
+			}
+			countDownLatch.await();
+
+
+			countDownLatch = new CountDownLatch(threadNum);
+			for (int i = 0; i < threadNum; i++) {
+				final List<String> files = getGroupFiles(goodfiles, i, threadNum);
+				new Thread(new GoodRun(countDownLatch, files)).start();
+			}
+			countDownLatch.await();
+
+			countDownLatch = new CountDownLatch(threadNum);
+			for (int i = 0; i < threadNum; i++) {
+				final List<String> files = getGroupFiles(orderfiles, i, threadNum);
+				new Thread(new OrderRun(countDownLatch, files)).start();
+			}
+			countDownLatch.await();
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private List<String> getGroupFiles(List<String> files, int group ,int readers){
 		//分给多个读线程
 		List<String> list = new ArrayList<String>();
 		for (int i = group; i < files.size(); i += readers ) {
@@ -28,127 +106,20 @@ public class ConstructSystem {
 		return list;
 	}
 
-	public void startOrderHandling(List<String> orderfiles, int readers, int handlers){
-		System.out.println("start handling orders!");
-		ReadBlockingQueue queues = new ReadBlockingQueue(handlers, 10000);
-		countDownLatch = new CountDownLatch(readers + handlers);
-
-		for (int i = 0; i < readers; i++) {
-			new Thread(new ReadFile(queues,
-					countDownLatch, 
-					getDealingFiles(orderfiles,i,readers))).start();
-		}
-
-		for (int i = 0; i < handlers; i++) {
-			LinkedBlockingQueue<String> queue = queues.getBlockQueue(i);
-			new Thread(new OrderHandler(agentGoodMapping, 
-					agentBuyerMapping, 
-					queue,
-					countDownLatch,
-					i,
-					readers)).start();
-		}
-		try {
-			countDownLatch.await();
-			//关闭所有ColumnFiles
-
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("end handling orders!");
-
-	}
-
-	public void startGoodHandling(List<String> goodfiles, int readers, int handlers){
-		System.out.println("start handling goods!");
-		ReadBlockingQueue queues = new ReadBlockingQueue(handlers, 10000);
-		countDownLatch = new CountDownLatch(readers + handlers);
-
-		for (int i = 0; i < readers; i++) {
-			new Thread(new ReadFile(queues,
-					countDownLatch, 
-					getDealingFiles(goodfiles,i,readers))).start();
-		}
-
-		for (int i = 0; i < handlers; i++) {
-			LinkedBlockingQueue<String> queue = queues.getBlockQueue(i);
-			new Thread(new GoodHandler(agentGoodMapping, 
-					queue, 
-					countDownLatch, 
-					i,
-					readers)).start();
-		}
-
-		try {
-			countDownLatch.await();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("end handling goods!");
-	}
-
-	public void startBuyerHandling(List<String> buyerfiles, int readers, int handlers){
-		System.out.println("start handling buyers!");
-		ReadBlockingQueue queues = new ReadBlockingQueue(handlers, 10000);
-		countDownLatch = new CountDownLatch(readers + handlers);
-
-		for (int i = 0; i < readers; i++) {
-			new Thread(new ReadFile(queues,
-					countDownLatch, 
-					getDealingFiles(buyerfiles,i,readers))).start();
-		}
-
-		for (int i = 0; i < handlers; i++) {
-			LinkedBlockingQueue<String> queue = queues.getBlockQueue(i);
-			new Thread(new BuyerHandler(agentBuyerMapping, 
-					queue, 
-					countDownLatch, 
-					i,
-					readers)).start();
-		}
-		try {
-			countDownLatch.await();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("end handling buyers!");
-	}
-
 
 	public static void main(String args[]){
-		ConstructSystem constructSystem = new ConstructSystem();
+
 		long startTime = System.currentTimeMillis();
+		List<String> buyerfiles = new ArrayList<String>();
+		buyerfiles.add("buyer_records.txt");
+		List<String> goodfiles = new ArrayList<String>();
+		goodfiles.add("good_records.txt");
+		List<String> orderfiles = new ArrayList<String>();
+		orderfiles.add("order_records.txt");
 
-		List<String> buyerFiles = new ArrayList<String>();
-		buyerFiles.add("buyer_records_1.txt");
-		buyerFiles.add("buyer_records_2.txt");
-		buyerFiles.add("buyer_records_3.txt");
-		constructSystem.startBuyerHandling(buyerFiles, 1, 1);
-		System.out.println("buyer table time:" + (System.currentTimeMillis() - startTime) / 1000);
+		ConstructSystem constructSystem = new ConstructSystem();
+		constructSystem.startHandling(buyerfiles, goodfiles, orderfiles, 3);
 
-		List<String> goodFiles = new ArrayList<String>();
-		buyerFiles.add("good_records_1.txt");
-		buyerFiles.add("good_records_2.txt");
-		buyerFiles.add("good_records_3.txt");
-		constructSystem.startGoodHandling(goodFiles, 1, 1);
-		System.out.println("good table time:" + (System.currentTimeMillis() - startTime) / 1000);
-
-//		final List<String> orderFiles = new ArrayList<String>();
-//		orderFiles.add("order_records1.txt");
-//		orderFiles.add("order_records2.txt");
-//		orderFiles.add("order_records3.txt");
-//		orderFiles.add("order_records4.txt");
-//		orderFiles.add("order_records5.txt");
-//		orderFiles.add("order_records6.txt");
-//		orderFiles.add("order_records7.txt");
-//		orderFiles.add("order_records8.txt");
-//		orderFiles.add("order_records9.txt");
-//		orderFiles.add("order_records10.txt");
-//		constructSystem.startOrderHandling(orderFiles, 1, 7);
-//		System.out.println("good table time:" + (System.currentTimeMillis() - startTime) / 1000);
-
+		System.out.println("order table time:" + (System.currentTimeMillis() - startTime) / 1000);
 	}
 }
--- src/main/java/com/alibaba/middleware/handlefile/GoodHandler.java
+++ src/main/java/com/alibaba/middleware/handlefile/GoodHandler.java
@@ -1,66 +1,63 @@
 package com.alibaba.middleware.handlefile;
 
-import java.util.concurrent.CountDownLatch;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class GoodHandler implements Runnable {
+public class GoodHandler{
 
 	AgentMapping agentGoodMapping;
-	LinkedBlockingQueue<String> queue;
-	CountDownLatch countDownLatch;
 	WriteFile goodfile;
-	int readers;
+	BufferedReader reader;
+	LinkedBlockingQueue<IndexItem> indexQueue;
 
-	public GoodHandler(AgentMapping agentGoodMapping,
-			LinkedBlockingQueue<String> queue,
-			CountDownLatch countDownLatch,
-			int threadId,
-			int readers) {
+	public GoodHandler(AgentMapping agentGoodMapping) {
 		this.agentGoodMapping = agentGoodMapping;
-		this.queue = queue;
-		this.countDownLatch = countDownLatch;
-		goodfile = new WriteFile("good/", "good_" + threadId + "_" , 10000000);
-		this.readers = readers;
+		goodfile = new WriteFile("good/", "good_", 10000000);
+		indexQueue = new LinkedBlockingQueue<IndexItem>();
 	}
 
-	public GoodHandler(AgentMapping agentGoodMapping,
-					   LinkedBlockingQueue<String> queue,
-					   int threadId,
-					   int readers) {
-		this.agentGoodMapping = agentGoodMapping;
-		this.queue = queue;
-		goodfile = new WriteFile("good/", "good_" + threadId + "_" , 10000000);
-		this.readers = readers;
-	}
-
-	public void handleRecord(String record){
-		String value = Utils.getValueFromRecord(record, "goodid");
-		Integer agentGoodId = agentGoodMapping.getValue(value);
+	private void handleGoodRecord(String record){
+		String goodid = Utils.getValueFromRecord(record, "goodid");
+		Integer agentGoodId = agentGoodMapping.getValue(goodid);
 		if (agentGoodId == null) {
-			agentGoodMapping.addEntry(value);
+			agentGoodMapping.addEntry(goodid);
+		}
+		//写入文件之前获取索引,放入阻塞队列中，Good表中对goodid键索引
+		IndexItem item = new IndexItem(goodid, goodfile.getFileName(), goodfile.getOffset(), IndexType.GOODFILE_GOODID);
+		try {
+			indexQueue.put(item);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		goodfile.writeLine(record);
 	}
 
-	@Override
-	public void run() {
-		while (true) {
+	public void HandleGoodFiles(List<String> files){
+		System.out.println("start good handling!");
+		for (String file : files) {
 			try {
-				String record = queue.take();
-				if (record.equals("END")) {
-					readers--;
-				}else {
-					handleRecord(record);
+				reader = new BufferedReader(new FileReader(file));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			String record = null;
+			try {
+				record = reader.readLine();
+				while (record != null) {
+					handleGoodRecord(record);
+					record = reader.readLine();
 				}
-				if (readers == 0) {
-					break;
-				}	
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
+				reader.close();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		goodfile.closeFile();
-		countDownLatch.countDown();
+		System.out.println("end good handling!");
 	}
 }
