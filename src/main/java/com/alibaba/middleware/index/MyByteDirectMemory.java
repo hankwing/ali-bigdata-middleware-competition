ed83f55f031b17399ca4ddb17aae6324d2d4e1cc
wxl <1450338535@qq.com>
2016-07-26 14:24:41 +0800

    temp

--- src/main/java/com/alibaba/middleware/index/MyByteDirectMemory.java
+++ src/main/java/com/alibaba/middleware/index/MyByteDirectMemory.java
@@ -89,7 +89,6 @@ public class MyByteDirectMemory {
 
 	public static byte[] int2byte(int res) {  
 		byte[] targets = new byte[4];  
-
 		targets[0] = (byte) (res & 0xff);// 最低位   
 		targets[1] = (byte) ((res >> 8) & 0xff);// 次低位   
 		targets[2] = (byte) ((res >> 16) & 0xff);// 次高位   
@@ -97,8 +96,7 @@ public class MyByteDirectMemory {
 		return targets;   
 	}   
 
-	public static int byte2int(byte[] res) {    
-
+	public static int byte2int(byte[] res) {
 		int targets = (res[0] & 0xff) | ((res[1] << 8) & 0xff00) // | 表示安位或   
 				| ((res[2] << 24) >>> 8) | (res[3] << 24);   
 		return targets;   
