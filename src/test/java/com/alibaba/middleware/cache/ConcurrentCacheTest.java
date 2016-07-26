f7bf645aeb6dd2bbc578f48d46ebf65c43f68f97
Jin Guodong <jelly.guodong.jin@gmail.com>
2016-07-26 20:13:15 +0800

    remove listener from cache

--- src/test/java/com/alibaba/middleware/cache/ConcurrentCacheTest.java
+++ src/test/java/com/alibaba/middleware/cache/ConcurrentCacheTest.java
@@ -1,7 +1,5 @@
 package com.alibaba.middleware.cache;
 
-import com.alibaba.middleware.conf.RaceConfig;
-
 /**
  * order-system-impl-master-cabe626d3eb46a36ae1f74a33ef3e8c7182536c7order-system-impl.git
  *
@@ -10,32 +8,63 @@ import com.alibaba.middleware.conf.RaceConfig;
 public class ConcurrentCacheTest {
 
     public static void main(String[] args) {
-        ConcurrentCache cache = ConcurrentCache.getInstance();
-
-        for (int i = 0; i < 10001; i++) {
-            cache.putInCache(i, String.valueOf(i), RaceConfig.TableName.BuyerTable);
-//            cache.putInCache(i, String.valueOf(i), RaceConfig.TableName.GoodTable);
-//            cache.putInCache(i, String.valueOf(i), RaceConfig.TableName.OrderTable);
-//            cacheMap.put(i, String.valueOf(i));
+//        ConcurrentCache cache = ConcurrentCache.getInstance();
+        try {
+            System.out.println("Sleep...");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        ConcurrentLinkedHashMap cacheMap = new ConcurrentLinkedHashMap.Builder<Integer, String>()
+                .initialCapacity(50)
+                .maximumWeightedCapacity(1000000)
+                .build();
+
+        try {
+            System.out.println("Sleep...");
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 11; i++) {
+//            List<byte[]> list = new ArrayList<byte[]>();
+//            cache.putInCache(i, String.valueOf(i), RaceConfig.TableName.BuyerTable);
+//            list.add("s".getBytes());
+//            cache.putInIdCache(i, list, RaceConfig.IdIndexType.BuyerIdToOrderOffsets);
+            cacheMap.put(i, String.valueOf(i));
+        }
+//        System.out.println(cache.getFromIdCache(1, RaceConfig.IdIndexType.BuyerIdToOrderOffsets));
 //        cache.putInCache(9, String.valueOf(9), RaceConfig.TableName.BuyerTable);
 //        System.out.println(cache.getFromCache(9, RaceConfig.TableName.BuyerTable));
 
 //        System.out.println("Force eviction");
 //        cache.forceEvict(1000000);
+        System.out.println(cacheMap.get(1));
+        System.out.println(cacheMap.get(0));
+        System.out.println(cacheMap.get(10));
 
+        try {
+            System.out.println("Sleep...");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 200000; i++) {
+            cacheMap.put(i+10, String.valueOf(i+10));
+        }
+        try {
+            System.out.println("Sleep...");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 2000000000; i++) {
+            cacheMap.put(i+200000, String.valueOf(i+200000));
+        }
 
-//        try {
-//            Thread.sleep(4000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        for (int i = 0; i < 2000000000; i++) {
-//            cache.putInCache(i, String.valueOf(i), RaceConfig.TableName.BuyerTable);
-//            cache.putInCache(i, String.valueOf(i), RaceConfig.TableName.GoodTable);
-//            cache.putInCache(i, String.valueOf(i), RaceConfig.TableName.OrderTable);
-//        }
-
-        System.out.println(cache.getSize());
+//        System.out.println(cache.getSize());
+        System.out.println(cacheMap.size());
     }
 }
