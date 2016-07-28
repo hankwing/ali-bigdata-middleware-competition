bdd06cd54bef80b3310206ea6e4dfba4dc467f37
Jin Guodong <jelly.guodong.jin@gmail.com>
2016-07-28 21:26:01 +0800

    make fifo cache concurrent

--- src/main/java/com/alibaba/middleware/cache/FIFOCache.java
+++ src/main/java/com/alibaba/middleware/cache/FIFOCache.java
@@ -6,6 +6,7 @@ import com.alibaba.middleware.index.HashBucket;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
@@ -13,35 +14,31 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  */
 public class FIFOCache {
 
-	private ReentrantReadWriteLock readWriteLock = null;
-    Queue<HashBucket> cache;
+    private ConcurrentLinkedQueue<HashBucket> cache;
     private long maxCapacity = RaceConfig.threIndexFileCapacity;
     private DiskHashTable context = null;
 
     public FIFOCache(DiskHashTable context) {
     	this.context = context;
-        cache = new LinkedList<HashBucket>();
-        readWriteLock = new ReentrantReadWriteLock(true);
+//        cache = new LinkedList<HashBucket>();
+        cache = new ConcurrentLinkedQueue<HashBucket>();
     }
 
     public boolean addBucket(HashBucket bucket) {
-    	//readWriteLock.writeLock().lock();
         if (cache.add(bucket)) {
             return true;
         }
-        //readWriteLock.writeLock().unlock();
         return false;
     }
 
     public void removeBucket() {
-    	//readWriteLock.writeLock().lock();
-        HashBucket bucket = cache.poll();
-        //readWriteLock.writeLock().unlock();
-        if (bucket != null) {
-            bucket.writeSelfWhenBuilding();
-            context.memRecordNum -= bucket.recordNum;
+        synchronized (cache) {
+            HashBucket bucket = cache.poll();
+            if (bucket != null) {
+                bucket.writeSelfWhenBuilding();
+                context.memRecordNum -= bucket.recordNum;
+            }
         }
-        
     }
 
     public boolean isReadyToRemove() { 
