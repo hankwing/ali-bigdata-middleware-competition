51d183932e2f8cfbe4a4e60e1f44ede1df9057c1
hankwing <hankwing@hotmail.com>
2016-07-29 20:04:29 +0800

    调整参数  不缓存桶到内存了

--- src/main/java/com/alibaba/middleware/conf/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/conf/RaceConfig.java
@@ -47,12 +47,12 @@ public class RaceConfig implements Serializable{
 	 * Cache pool config
 	 * */
     public static int cacheInitCapacity = 5000;                    // ConcurrentCache中每个队列的初始大小
-    public static int cacheMaxCapacity = 8000;                    // ConcurrentCache中每个队列的最大大小
+    public static int cacheMaxCapacity = 10000;                    // ConcurrentCache中每个队列的最大大小
     public static int bucketCapcity = 100;                        // 桶CACHE的最大上限
     public static int bucketRemoveNum = 10;                        // 每次桶的CACHE达到上限后删除一定量的桶
 
 
-	public static int hash_index_block_capacity = 5000;			// 一个索引桶里的数据量
+	public static int hash_index_block_capacity = 500;			// 一个索引桶里的数据量
 
 	public static String booleanTrueValue = "true";
 	public static String booleanFalseValue = "false";
